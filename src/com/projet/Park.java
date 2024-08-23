e961219ac4d549e2cb552d49e1aa530c9f994b06
Randriharimalala <hei.finoana.8@gmail.com>
2024-08-23 10:32:26 +0300

    feat: add all methods that need to override

--- src/com/projet/Park.java
+++ src/com/projet/Park.java
@@ -2,12 +2,14 @@ package com.projet;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Park extends Place {
     private List<Hotel> hotelList;
-    public Park(String name, GeographicCoordinate geographicCoordinate, Text description, List<Hotel> hotelList) {
-        super(name, geographicCoordinate, description);
+    private List<CustomerOpinion> customerOpinionList = new ArrayList<>();
+    public Park(String name, GeographicCoordinate geographicCoordinate, Text description, List<Hotel> hotelList,List<CustomerOpinion> customerOpinionList) {
+        super(name, geographicCoordinate, description,customerOpinionList);
         this.hotelList = hotelList;
     }
     public List<Hotel> getHotelList() {
@@ -16,5 +18,52 @@ public class Park extends Place {
     public void setHotelList(List<Hotel> hotelList) {
         this.hotelList = hotelList;
 
+    }
+
+    public List<CustomerOpinion> getCustomerOpinionList() {
+        return customerOpinionList;
+    }
+
+    public void setCustomerOpinionList(List<CustomerOpinion> customerOpinionList) {
+        this.customerOpinionList = customerOpinionList;
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public GeographicCoordinate getGeographicCoordinate() {
+        return super.getGeographicCoordinate();
+    }
+
+    @Override
+    public void setGeographicCoordinate(GeographicCoordinate geographicCoordinate) {
+        super.setGeographicCoordinate(geographicCoordinate);
+    }
+
+    @Override
+    public Text getDescription() {
+        return super.getDescription();
+    }
+
+    @Override
+    public void setDescription(Text description) {
+        super.setDescription(description);
+    }
+
+    public Park(String name, GeographicCoordinate geographicCoordinateList, Text description, List<CustomerOpinion> customerOpinionList) {
+        super(name, geographicCoordinateList, description, customerOpinionList);
+    }
+
+    @Override
+    public void addOpinion(List<CustomerOpinion> customerOpinionList) {
+        super.addOpinion(customerOpinionList);
     }
 }
